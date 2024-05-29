d7749468a29407c383c4b60268c288fd7191e939
Brookemooreeeee <brookemooreeeee@gmail.com>
2024-05-29 01:07:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) 
+  {
+    // Declare the constant for conversion
+    const double CENTIMETERS_PER_INCH = 2.54;
+    // Variable to assign inch value
+    double inches = 5;
+    // Variable that calculates and stores the value for centimeters
+    double centimeters = inches * CENTIMETERS_PER_INCH;
+    // Displays inches to centimeters
+    Console.WriteLine ("{0} inches is {1} centimeters", inches, centimeters);
+    }
+}
